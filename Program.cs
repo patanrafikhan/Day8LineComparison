364be1e585a4f1754b87438f51f16af0bfa5dba5
patanrafikhan <121918502+patanrafikhan@users.noreply.github.com>
2023-01-31 21:43:21 +0530

    UC1 Line Comparison Program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace LineComparison
+namespace LinecomaparisonUC1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comaparison");
+            Console.WriteLine("Welcome to Line Comparison");
+            LengthofLine();
         }
+        public static void LengthofLine() 
+        {
+            
+            Console.WriteLine("Enter First Point ");
+            Console.WriteLine("X1=");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y1=");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Enter the Second Point");
+            Console.WriteLine("X2=");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y2=");
+            double y2= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("---------------------------");
+
+            double LengthofLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of a Line is :{0}",LengthofLine);
+        }
+        
     }
 }
